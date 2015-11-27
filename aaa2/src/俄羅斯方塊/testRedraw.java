4794ab8a7cd3dce237210a876979de50b97d8435
KovichChiu <kovich850329@gmail.com>
2015-11-28 00:01:25 +0800

    JavaTetris ver 0.1.0
    103-2 Practice Program Tetris.

---
+++ aaa2/src/俄羅斯方塊/testRedraw.java
@@ -0,0 +1,21 @@
+package «XÃ¹´µ¤è¶ô;
+
+import java.awt.event.*;
+
+import javax.swing.Timer;
+
+public class testRedraw implements Runnable {
+	private Timer t1;
+
+	public testRedraw(testlabel label) {
+		t1 = new Timer(1, new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				label.reDraw();
+			}
+		});
+	}
+
+	public void run() {
+		t1.start();
+	}
+}
